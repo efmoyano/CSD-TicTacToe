04f9e268c4be24f405ba24aad41341ba904032ee
anrocan <vagrant@kleer-csd-box>
2017-04-21 12:11:40 -0300

    Logica FE Presionar casillero

--- features/TicTacToe.feature
+++ features/TicTacToe.feature
@@ -4,4 +4,10 @@ Scenario:
   Given Start app
   Then I must see "Tic Tac Toe"
 
+Scenario: Presiona Boton 1
+     Given Start app
+	When Presiono el Boton 1 
+	Then Boton 1=X
+
+
 
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,4 +6,12 @@ Then(/^I must see "(.*?)"$/) do |message|
   last_response.body.should =~ /#{message}/m
 end
 
+When(/^Presiono el Boton 1$/) do
+  click_button("1")
+end
+
+Then(/^Boton (\d+)=X$/) do |marca|
+  last_response.body.should =~/#{marca}/m
+end
+
 
